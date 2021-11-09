b3022f0cf7ae26ba40c47a109ccb9926d7c30ff6
Isak Pålsson <ispalsson@gmail.com>
2021-11-09 18:39:14 +0100

    !!

--- kursolle_moment_struktur-master/moment02/inlamning02a.py
+++ kursolle_moment_struktur-master/moment02/inlamning02a.py
@@ -2,10 +2,10 @@
 
 #Definerar variabler aka pi och radie. Valde att ha en float istället för en int för att det blir mer precist
 pi = 3.14
-radie = float(input("Skriv radie med decimaler aka med pungkt: "))
+radie = float(input("Skriv radie med decimaler aka med punkt: "))
 
-#Här räknar jag ut och printar dem olika delarna. Jag andvänder \u00b2 på arean så att det ska se fint ut. 
-#Samtidigt så andvänder jag {:.2f} för att få 2 decimaler istälet för fler eller färre.
+#Här räknar jag ut och printar dem olika delarna. Jag använder \u00b2 på arean så att det ska se fint ut. 
+#Samtidigt så använder jag {:.2f} för att få 2 decimaler istälet för fler eller färre.
 #Jag gör uträkningarna i format för att det minskar antalet rader jämfört med att definera nya variablar och sedan printa dem.
 print("radie: {:.2f}cm".format(radie))
 print("diameter: {:.2f}cm".format(radie*2))
