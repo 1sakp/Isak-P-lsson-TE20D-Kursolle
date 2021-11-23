0a4a323219ee5e7382f6b5b0439c6e1ef5f90d58
Isak Pålsson <ispalsson@gmail.com>
2021-11-23 11:07:29 +0100

    !

--- kursolle_moment_struktur-master/moment03/inlamning03a.py
+++ kursolle_moment_struktur-master/moment03/inlamning03a.py
@@ -1,3 +1,4 @@
-
 lon = int(input("Skriv brutto inkomst/månad i hela kr:    "))
 
+print("månadslön: {:.0f}\nkomunalskatt: {:.0f}\nlandtingsskatt: {:.0f}\nKvar efter skatt: {:.0f}".format(lon, 
+(lon/100)*21.36, (lon/100)*11.48, lon-(((lon/100)*11.48)+((lon/100)*21.36))))
