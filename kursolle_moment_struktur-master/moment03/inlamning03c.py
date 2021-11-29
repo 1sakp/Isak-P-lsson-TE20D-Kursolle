499c508c23460e44009d63e845dc9c062abb7ec8
Isak Pålsson <ispalsson@gmail.com>
2021-11-29 10:18:26 +0100

    !!!

--- kursolle_moment_struktur-master/moment03/inlamning03c.py
+++ kursolle_moment_struktur-master/moment03/inlamning03c.py
@@ -4,7 +4,7 @@ arslon = lon*12
 if arslon >= 19248:
     if arslon > 675700:
         print(print("månadslön: {:.0f}    (årslön: {:.0f})\nkomunalskatt: {:.0f}\nlandtingsskatt: {:.0f}\nStadsskatt: {:.0f}\nKvar efter skatt: {:.0f}".format(lon, 
-        arslon, (lon/100)*21.36, (lon/100)*11.48, (lon/100)*25, lon-(((lon/100)*11.48)+((lon/100)*21.36)+(lon/100)*20)+(lon/100)*5)))
+        arslon, (lon/100)*21.36, (lon/100)*11.48, (lon/100)*25, lon-((((((lon/100)*11.48))+((lon/100)*21.36))+(lon/100)*20))+(lon/100)*5)))
         
     elif arslon > 468700:
         print(print("månadslön: {:.0f}    (årslön: {:.0f})\nkomunalskatt: {:.0f}\nlandtingsskatt: {:.0f}\nStadsskatt: {:.0f}\nKvar efter skatt: {:.0f}".format(lon, 
