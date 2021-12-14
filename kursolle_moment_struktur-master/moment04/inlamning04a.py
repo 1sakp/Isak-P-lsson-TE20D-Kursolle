57da10aa1e133604a832a5ada5df81f515bccc8c
Isak Pålsson <ispalsson@gmail.com>
2021-12-14 10:28:42 +0100

    !!

--- kursolle_moment_struktur-master/moment04/inlamning04a.py
+++ kursolle_moment_struktur-master/moment04/inlamning04a.py
@@ -1,13 +1,13 @@
-num1 = int(input("Rectangle side:   "))
-num2 = int(input("Rectangle side:   "))
+num1 = int(input("Rektangle sida:   "))
+num2 = int(input("Rektangle sida:   "))
 x = 1
 
 print(f"area = {num1 * num2} ")
 
 if num1 == num2:
-    print("is kvadrat")
+    print("den är kvadratisk")
 else:
-    print("is rektangel")
+    print("den är en rektangel")
 
 print("{0:^10s}|{1:^10s}".format("Höjd","volymen"))
 for i in range(1, 11):
