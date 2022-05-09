460b046e56a7c5a56d3ce256995989d6c5eb0289
Isak Pålsson <ispalsson@gmail.com>
2022-05-09 21:29:43 +0200

    !!

--- kursolle_moment_struktur-master/bankapplikation/inlamning_bankapp.py
+++ kursolle_moment_struktur-master/bankapplikation/inlamning_bankapp.py
@@ -1,38 +1,58 @@
 import tkinter as tk
 
+bal_global = str(0)
+
+def balance():
+    master = tk.Tk()
+
+    tk.Label(master, text=bal_global).grid(row=2)
+
+    master.mainloop()
+
 def pw_check(name, pasword):
-    global check
-    name_l = name
-    with open (str(name_l)+".txt", "r") as file:
+    global bal_global
+    with open (str(name)+".txt", "r") as file:
         pword = file.readline()
-        if pword == pasword:
-            check = True
+        print(pword)
+        if pword.strip() == pasword:
+            bal_global = file_open_login(name)
+            balance()
         else:
-            check = False
+            exit()
         file.close()
 
 def file_open_signup(name, pasword, balance):
-    name_l = name
-    with open (str(name_l)+".txt", "w+") as file:
+    with open (str(name)+".txt", "w+") as file:
         file.write(str(pasword) + "\n" + str(balance))
         file.close()
 
+def file_open_login(name):
+    with open (str(name)+".txt", "r") as file:
+        bal = file.readlines()[1:]
+        return bal
+
 def login():
     master = tk.Tk()
-    tk.Label(master, text="Username").grid(row=0)
-    tk.Label(master, text="Pasword").grid(row=1)
-    tk.Label(master, text="bal: ").grid(row=2)
-
+    tk.Label(master, text="Username: ").grid(row=0)
+    tk.Label(master, text="Pasword: ").grid(row=1)
+    
     e1 = tk.Entry(master)
     e2 = tk.Entry(master)
-
+    
+    def pw_check_local():
+        name_1 = e1.get()
+        pasword_2 = e2.get()
+        pw_check(name_1, pasword_2)
+    
+    e3 = tk.Button(master, width="10", command= lambda: pw_check_local())
+    
     e1.grid(row=0, column=1)
     e2.grid(row=1, column=1)
+    e3.grid(row=2, column=1)
 
     master.mainloop()
 
 def signup():
-    
     master = tk.Tk()
     tk.Label(master, text="Username").grid(row=0)
     tk.Label(master, text="Pasword").grid(row=1)
@@ -41,7 +61,16 @@ def signup():
     e1 = tk.Entry(master)
     e2 = tk.Entry(master)
     e3 = tk.Entry(master)
-    e4 = tk.Button(master, width="10", command= lambda: file_open_signup(e1, e2, e3))
+    
+    def file_open_signup_local():
+        name_1 = e1.get()
+        pasword_2 = e2.get()
+        balance_3 = e3.get()
+        file_open_signup(name_1, pasword_2, balance_3)
+        
+        
+    
+    e4 = tk.Button(master, width="10", command= lambda: file_open_signup_local())
 
     e1.grid(row=0, column=1)
     e2.grid(row=1, column=1)
