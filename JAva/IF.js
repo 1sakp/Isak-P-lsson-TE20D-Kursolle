9ac7f1d7ef76d6eed5ed32e4fda931ec85eb64d0
Isak Pålsson <ispalsson@gmail.com>
2022-09-14 13:56:38 +0200

    u

--- JAva/IF.js
+++ JAva/IF.js
@@ -9,4 +9,16 @@ if (input > 20 || input < 10){
     console.log("innanför intervall")
 };
 
-//3
+//4
+let num_input1 = prompt("Skriv 1 nummer: ");
+let num_input2 = prompt("Skriv 1 nummer: ");
+let num_input3 = prompt("Skriv 1 nummer: ");
+
+let arr_num = []
+arr_num.push(num_input1, 1);
+arr_num.push(num_input2, 2);
+arr_num.push(num_input3, 3);
+
+arr_num.sort()
+
+console.log(arr_num)
